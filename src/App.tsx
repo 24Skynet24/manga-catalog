612a76256b1b4aa942ac3dad67caeb6bec98e782
24Skynet24 <ksk836684@gmail.com>
2024-07-12 01:24:18 +0400

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-function App() {
+import {Route, Routes} from "react-router-dom"
+import HomePage from "./pages/home"
 
-
-  return (
-    <>
-
-    </>
-  )
+const App = () => {
+    return (
+        <>
+            <Routes>
+                <Route index path="/" element={<HomePage/>}/>
+                {/*<Route path="/page" element={<Page/>} />*/}
+            </Routes>
+        </>
+    )
 }
 
 export default App
