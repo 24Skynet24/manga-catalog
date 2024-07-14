ebc06bd7ccdbaec76d02823e4f5a604021de7096
24Skynet24 <ksk836684@gmail.com>
2024-07-14 22:07:34 +0400

    add manga type

---
+++ src/types/mangaType.ts
@@ -0,0 +1,15 @@
+export interface MangaType {
+    authors: string[],
+    create_at: number | string,
+    genres: string[]
+    id: string,
+    nsfw: boolean,
+    status: string,
+    sub_title: string,
+    summary: string,
+    thumb: string,
+    title: string,
+    total_chapter: number | string,
+    type: string,
+    update_at: number | string,
+}
