0c7e1fd1ea707d1e5c73fd9750f314eb52487539
24Skynet24 <ksk836684@gmail.com>
2024-07-14 19:25:26 +0400

    add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import {Route, Routes} from "react-router-dom"
 import HomePage from "./pages/home"
+import Header from "./components/Header";
 
 const App: React.FC = () => {
     return (
         <>
+            <Header/>
             <Routes>
                 <Route index path="/" element={<HomePage/>}/>
                 {/*<Route path="/page" element={<Page/>} />*/}
