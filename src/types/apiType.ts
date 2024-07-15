6ead80443c7b731c38e5a867d9da64a316b9e755
24Skynet24 <ksk836684@gmail.com>
2024-07-16 01:45:02 +0400

    refactor add new ApiType field

---
+++ src/types/apiType.ts
@@ -0,0 +1,11 @@
+export interface ApiArgs {
+    url: string,
+    query?: ApiObjectsType
+    headers?: ApiObjectsType
+    method?: string,
+    body?: string
+}
+
+interface ApiObjectsType {
+    [key: string]: string | number
+}
