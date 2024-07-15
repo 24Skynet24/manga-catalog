f5616a6bc11f557f9967ae82d895aefa87d108d7
24Skynet24 <ksk836684@gmail.com>
2024-07-16 01:47:38 +0400

    refactor output manga cards

--- src/modules/LatestManga/index.tsx
+++ src/modules/LatestManga/index.tsx
@@ -1,16 +1,24 @@
-import React, {useEffect, useState} from 'react'
+import {useEffect, useState} from 'react'
 import services from "../../services"
 import {MangaType} from "../../types/mangaType"
 import MangaCard from "../../components/MangaCard";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "./latestManga.scss"
+import collectingMangaCover from "../../utils/collectingMangaCover";
 
 const LatestManga = () => {
-    const [latestList, setList] = useState<MangaType[]>([])
+    const [mangaList, setList] = useState<MangaType[]>([])
     useEffect(() => {
         const getFetch = async () => {
-            const res = await services.MangaServices.getMangaLatestList()
-            if (res) setList(res)
+            const res = await services.MangaServices.getMangaList()
+            const coverArts = collectingMangaCover(res)
+            const mangaCovers = await services.MangaServices.getMangaCover(coverArts)
+            res.map(manga => {
+                let rel = ""
+                manga.relationships.map(relationship => { if (relationship.type === "cover_art") rel = relationship.id })
+                mangaCovers.map(el => { if (rel === el.id) manga.attributes.img = el.attributes.fileName })
+            })
+            setList(res)
         }
         getFetch()
     }, [])
@@ -21,7 +29,7 @@ const LatestManga = () => {
                 <h3>Latest Updates</h3>
                 <div className="latest_updates_container">
                     <Swiper spaceBetween={10} slidesPerView={6}>
-                        {latestList.map((el: MangaType, id: number) => {
+                        {mangaList.map((el: MangaType, id: number) => {
                             return <SwiperSlide key={id}><MangaCard {...el}/></SwiperSlide>
                         })}
                     </Swiper>
