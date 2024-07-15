76eb429009bdf6b126dc50688d3ac00f61978626
24Skynet24 <ksk836684@gmail.com>
2024-07-16 01:46:44 +0400

    refactor add new MangaType fields

--- src/types/mangaType.ts
+++ src/types/mangaType.ts
@@ -1,15 +1,66 @@
 export interface MangaType {
-    authors: string[],
-    create_at: number | string,
-    genres: string[]
-    id: string,
-    nsfw: boolean,
-    status: string,
-    sub_title: string,
-    summary: string,
-    thumb: string,
-    title: string,
-    total_chapter: number | string,
-    type: string,
-    update_at: number | string,
+    attributes: MangaAttributesType
+    id: string
+    relationships: MangaRelationshipsType[]
+    type: string
+}
+
+export interface MangaCoverType {
+    attributes: {
+        description: string,
+        volume: string | number,
+        fileName: string,
+        locale: string,
+        createdAt: string,
+        updatedAt: string,
+        version: string | number
+    }
+    id: string
+    relationships: MangaStringObjects[]
+    type: string
+}
+
+interface MangaRelationshipsType {
+    id: string
+    type: string
+}
+
+interface MangaAttributesType {
+    title: MangaStringObjects
+    altTitles: MangaStringObjects[]
+    description: MangaStringObjects
+    isLocked: boolean
+    links: MangaStringObjects
+    originalLanguage: string
+    lastVolume: string
+    lastChapter: string
+    publicationDemographic: string
+    status: string
+    year: number
+    contentRating: string
+    tags: MangaTagsType[]
+    state: string
+    chapterNumbersResetOnNewVolume: boolean
+    createdAt: string
+    updatedAt: string
+    version: number
+    availableTranslatedLanguages: string[]
+    latestUploadedChapter: string
+    img?: string
+}
+
+interface MangaTagsType {
+    attributes: {
+        name: MangaStringObjects
+        description: MangaStringObjects
+        group: string
+        version: number | string
+    }
+    id: string | number
+    relationships: MangaRelationshipsType[],
+    type: string
+}
+
+interface MangaStringObjects {
+    [key: string]: string;
 }
