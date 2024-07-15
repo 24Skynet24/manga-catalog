8fdef1f0003c589d03ac8d89be72b1f60dcb9cab
24Skynet24 <ksk836684@gmail.com>
2024-07-16 01:47:08 +0400

    refactor add new MangaServices methods

--- src/services/MangaServices.ts
+++ src/services/MangaServices.ts
@@ -1,14 +1,30 @@
 import API from "../plugins/API"
+import {MangaCoverType, MangaType} from "../types/mangaType";
 
 export default {
-    getMangaLatestList: async () => {
+    getMangaList: async (): Promise<MangaType[]> => {
         try {
-            const data = await API({url: "/latest"})
+            const data = await API({url: "/manga", query: {limit: 25}})
             return data.data
         }
         catch (err) {
             console.error("ERROR Latest List " + err)
-            return []
+            throw new Error(err)
         }
-    }
+    },
+
+    getMangaCover: async (cover_arts: string[]): Promise<MangaCoverType[]> => {
+        let imagesQuery: string = ""
+        cover_arts.map(el => {imagesQuery += `ids[]=${el}&`})
+        imagesQuery += "limit=25"
+
+        try {
+            const data = await API({url: `/cover?${imagesQuery}`})
+            return data.data
+        }
+        catch (err) {
+            console.error("ERROR Latest List " + err)
+            throw new Error(err)
+        }
+    },
 }
