b530578001ecd10d5d7ee0355a589843eb2d9bdd
Albert <albert.mnac@gmail.com>
2024-07-31 17:54:51 +0400

    refactor images

--- src/modules/TopTags/staticData.ts
+++ src/modules/TopTags/staticData.ts
@@ -1,8 +1,9 @@
 import {TagCardType} from "../../types/mangaTagType";
+
 import comedyImg from "@/assets/img/tags/comedy.png"
-import fantasyImg from "@/assets/img/tags/fantasy.jpg"
+import fantasyImg from "@/assets/img/tags/fantasy.png"
 import dramaImg from "@/assets/img/tags/drama.jpg"
-import romanceImg from "@/assets/img/tags/romance.jpg"
+import romanceImg from "@/assets/img/tags/romance.png"
 
 const topTags: TagCardType[] = [
     {
