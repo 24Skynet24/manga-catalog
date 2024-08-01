a66dc47d8a8a45fb901b54bfd64ff298aec392e1
Albert <albert.mnac@gmail.com>
2024-08-01 16:11:17 +0400

    refactor MangaCard

--- src/components/MangaCard/index.tsx
+++ src/components/MangaCard/index.tsx
@@ -1,16 +1,16 @@
 import "./mangaCard.scss"
 import {MangaType} from "../../types/mangaType";
 import {Link} from "react-router-dom";
+import {imgUrl} from "../../utils/getImgUrl";
 
 const MangaCard = ({id, attributes}: MangaType) => {
-    const imgUrl: string = `url(https://uploads.mangadex.org/covers/${id}/${attributes.img})`
     return (
         <article className="manga_card" title={attributes.title.en}>
             <Link to={`/title/${id}`}>
                 <div
                     className="manga_card_img"
                     style={{
-                        backgroundImage: imgUrl,
+                        backgroundImage: imgUrl(id, attributes.img),
                     }}
                 />
             </Link>
