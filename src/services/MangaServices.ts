438d30832138069bb45713f6d7471481afa5c862
Albert <albert.mnac@gmail.com>
2024-08-05 04:09:00 +0400

    add new method to Manga Services

--- src/services/MangaServices.ts
+++ src/services/MangaServices.ts
@@ -1,6 +1,5 @@
 import API from "../plugins/API"
-import {MangaCoverType, MangaType} from "../types/mangaType";
-import {MangaTagType} from "../types/mangaTagType";
+import {MangaCoverType, MangaTitleChaptersType, MangaType, MangaTagType} from "../types/mangaType";
 import {getToDay} from "../utils/datesHelper";
 
 export default {
@@ -54,6 +53,42 @@ export default {
         }
         catch (err) {
             console.error("ERROR Manga List " + err)
+            throw new Error(err)
+        }
+    },
+
+    getMangaTitle: async (mangaId: string | undefined): Promise<MangaType>=> {
+        try {
+            const data = await API({url: `/manga/${mangaId}`})
+            return data.data
+        }  catch (err) {
+            console.error("ERROR Manga title " + err)
+            throw new Error(err)
+        }
+    },
+
+    getTitleCover: async (cover_id: string): Promise<MangaCoverType> | never => {
+        try {
+            const data = await API({url: `/cover/${cover_id}`})
+            return data.data
+        }  catch (err) {
+            console.error("ERROR Title Cover " + err)
+            throw new Error(err)
+        }
+    },
+
+    getTitleChapters: async (mangaId: string, offset: number = 0): Promise<MangaTitleChaptersType[]> | never => {
+        const query = {
+            offset: offset,
+            manga: mangaId,
+            limit: 100,
+            "translatedLanguage[]": ["en"]
+        }
+        try {
+            const data = await API({url: `/chapter`, query: query})
+            return data.data
+        }  catch (err) {
+            console.error("ERROR Title Chapters " + err)
             throw new Error(err)
         }
     }
