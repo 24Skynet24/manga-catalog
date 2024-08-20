ef9b976b4b8f07f4e7a04d41c0df5b98ae0799b4
Albert <albert.mnac@gmail.com>
2024-08-20 17:52:12 +0400

    add Catalog filters

--- src/modules/Catalog/index.tsx
+++ src/modules/Catalog/index.tsx
@@ -1,7 +1,41 @@
 import "./catalog.scss"
-import {FormControlLabel, Checkbox} from "@mui/material";
+import {FormControlLabel, Checkbox} from "@mui/material"
+import {useEffect, useState} from "react"
+import services from "../../services"
+import {MangaTagType} from "../../types/mangaType"
+import {catalogFilters} from "./data"
 
-const Catalog = ({tags, year, contentRating}) => {
+const colors: string[] = ["#B32525"]
+
+const CatalogListItem = ({name, color}) => {
+    return (
+        <>
+            <li>
+                <FormControlLabel
+                    label={name}
+                    control={<Checkbox sx={{
+                        '& .MuiSvgIcon-root': { fontSize: 20 },
+                        color: colors[color],
+                        '&.Mui-checked': {
+                            color: colors[color],
+                        },
+                    }}/>}
+                />
+            </li>
+        </>
+    )
+}
+
+const Catalog = ({tagsSelect, yearSelect, contentRatingSelect}) => {
+    const [tags, setTags] = useState<MangaTagType[]>([])
+    useEffect(() => {
+        const getTags = async () => {
+            const res = await services.MangaServices.getMangaTagsList()
+            res.sort((a,b) => a.attributes.name.en.localeCompare(b.attributes.name.en))
+            setTags(res)
+        }
+        getTags().then(r => r)
+    }, [])
     return (
         <section className="module_section">
             <h1>Catalog</h1>
@@ -10,30 +44,38 @@ const Catalog = ({tags, year, contentRating}) => {
                 <aside className="filters">
                     <h2>Filters</h2>
                     <form>
-                        <ul className="flex flex-col">
+                        <ul className="flex flex-col filters_list">
                             <li className="filters_item">
                                 <h5 className="filters_item_name">Year</h5>
-                                <div className="filters_item_group">
-                                    <FormControlLabel
-                                        label="2024"
-                                        control={<Checkbox sx={{
-                                                '& .MuiSvgIcon-root': { fontSize: 20 },
-                                                color: "#B32525",
-                                                '&.Mui-checked': {
-                                                    color: "#B32525",
-                                                },
-                                            }}/>}
-                                    />
-                                </div>
+                                <ul className="filters_item_row">
+                                    {catalogFilters.years.map((el, id) => {
+                                        return <CatalogListItem name={el} key={id} color={0}/>
+                                    })}
+                                </ul>
                             </li>
                             <li className="filters_item">
-                                <h5>Status</h5>
+                                <h5 className="filters_item_name">Status</h5>
+                                <ul className="filters_item_row">
+                                    {catalogFilters.statuses.map((el, id) => {
+                                        return <CatalogListItem name={el} key={id} color={0}/>
+                                    })}
+                                </ul>
                             </li>
                             <li className="filters_item">
-                                <h5>Content rating</h5>
+                                <h5 className="filters_item_name">Content rating</h5>
+                                <ul className="filters_item_row">
+                                    {catalogFilters.contentRatings.map((el, id) => {
+                                        return <CatalogListItem name={el} key={id} color={0}/>
+                                    })}
+                                </ul>
                             </li>
                             <li className="filters_item">
-                                <h5>Tags</h5>
+                                <h5 className="filters_item_name">Tags</h5>
+                                <ul className="filters_item_list">
+                                    {tags.map((el, id) => {
+                                        return <CatalogListItem name={el.attributes.name.en} key={id} color={0}/>
+                                    })}
+                                </ul>
                             </li>
                         </ul>
                     </form>
